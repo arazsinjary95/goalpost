9ee6e99e0fc0bb4e28c16568c99a96e5cc24c7ff
Araz Sinjary <araz@Arazs-MBP.Home>
2018-07-11 22:08:30 +0300

    displaying static goalcells in uitableview/creating goaltype enum

---
+++ goalpost/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  goalpost
+//
+//  Created by Araz Sinjary on 7/11/18.
+//  Copyright © 2018 Araz Sinjary. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
