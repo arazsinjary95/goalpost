bf2cd552536e34e743b553d5a5d6817294c590ac
Araz Sinjary <araz@Arazs-MBP.Home>
2018-07-12 20:55:04 +0300

    creating uiview/uibutton extensioin

--- goalpost/Controller/CreateGoalVC.swift
+++ goalpost/Controller/CreateGoalVC.swift
@@ -15,15 +15,26 @@ class CreateGoalVC: UIViewController {
     @IBOutlet weak var longTermBtb: UIButton!
     @IBOutlet weak var nextBtn: UIButton!
     
+    var goalType: GoalType = .shortTerm
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        nextBtn.bindToKeyboard()
+        shortTermBtb.setSelectedColor()
+        longTermBtb.setDeselectedColor()
     }
 
     @IBAction func shortTermBtnWasPressed(_ sender: Any) {
+        goalType = .shortTerm
+        shortTermBtb.setSelectedColor()
+        longTermBtb.setDeselectedColor()
     }
     
     @IBAction func longTermBtnWasPressed(_ sender: Any) {
+        goalType = .longTerm
+        longTermBtb.setSelectedColor()
+        shortTermBtb.setDeselectedColor()
     }
     
     @IBAction func nexrBtnWasPressed(_ sender: Any) {
